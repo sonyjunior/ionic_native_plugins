2a40579fcb2c8c4590dd9fb34186dea9cd69ca54
sonyjunior <antonio.rolim2@etec.sp.gov.br>
2021-04-08 19:14:08 -0300

    enviando
    tentando enviar

---
+++ UseCamera/src/app/home/home.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {Plugins, CameraResultType, CameraSource} from '@capacitor/core'; 
+import { fromEventPattern} from 'rxjs';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  photo: SafeResourceUrl;
+  
+  constructor(private sanitizer: DomSanitizer) {}
+  async takePicture(){
+    const image = await Plugins.Camera.getPhoto({
+    quality: 100,
+    allowEditing: false,
+    resultType: CameraResultType.DataUrl,
+    source: CameraSource.Camera
+    });
+
+  this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
+
+}
+}
